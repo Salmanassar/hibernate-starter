bfc4bf3b208280b68c1487ec946e16f1d7c8cf2b
Professional <folknerolek@gmail.com>
2021-12-26 11:05:03 +0100

    Create JSONB in  HibernateStarter

--- src/main/java/com/hibernate/HibernateStarter.java
+++ src/main/java/com/hibernate/HibernateStarter.java
@@ -4,6 +4,7 @@ import com.hibernate.entity.Birthday;
 import com.hibernate.coverter.BirthdayConvertor;
 import com.hibernate.entity.Role;
 import com.hibernate.entity.User;
+import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -15,6 +16,7 @@ public class HibernateStarter {
     public static void main(String[] args) throws SQLException {
         Configuration configuration = new Configuration();
         configuration.addAttributeConverter(new BirthdayConvertor(),true);
+        configuration.registerTypeOverride(new JsonBinaryType());
         configuration.configure("hibernate.cfg.xml");
 //If you need to use annotations without hibernate.cfg.xml use the java code below
 //configuration.addAnnotatedClass(User.class);
@@ -27,6 +29,12 @@ public class HibernateStarter {
                     .lastName("Alexeev")
                     .birthDay(new Birthday(LocalDate.of(1989, 3, 10)))
                     .role(Role.USER)
+                    .info("""
+                            {
+                            "name": "Alex",
+                            "id": 15 
+                            }                           
+                            """)
                     .build();
             session.saveOrUpdate(user);
             session.getTransaction().commit();

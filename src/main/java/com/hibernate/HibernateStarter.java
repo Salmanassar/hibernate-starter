7f4f07447bfeb18ab8265791c121f50083459810
Professional <folknerolek@gmail.com>
2021-12-29 02:33:08 +0100

    different between refresh and merge(dirty) methods in the Persistent context (firs level cash)

--- src/main/java/com/hibernate/HibernateStarter.java
+++ src/main/java/com/hibernate/HibernateStarter.java
@@ -1,43 +1,47 @@
 package com.hibernate;
 
 import com.hibernate.entity.Birthday;
-import com.hibernate.coverter.BirthdayConvertor;
 import com.hibernate.entity.Role;
 import com.hibernate.entity.User;
-import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
+import com.hibernate.util.HibernateConfigurationUtil;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
 
 import java.sql.SQLException;
 import java.time.LocalDate;
 
 public class HibernateStarter {
     public static void main(String[] args) throws SQLException {
-        Configuration configuration = new Configuration();
-        configuration.addAttributeConverter(new BirthdayConvertor(),true);
-        configuration.registerTypeOverride(new JsonBinaryType());
-        configuration.configure("hibernate.cfg.xml");
+        User user = User.builder()
+                .userName("alexx@gmail.com")
+                .firstName("Alex")
+                .lastName("Alexeev")
+                .birthDay(new Birthday(LocalDate.of(1989, 3, 10)))
+                .role(Role.USER)
+                .info("""
+                        {
+                        "name": "Alex",
+                        "id": 15
+                        }
+                        """)
+                .build();
+
 //If you need to use annotations without hibernate.cfg.xml use the java code below
 //configuration.addAnnotatedClass(User.class);
-        try (SessionFactory sessionFactory = configuration.buildSessionFactory()) {
-            Session session = sessionFactory.openSession();
-            session.beginTransaction();
-            User user = User.builder()
-                    .userName("alexx@gmail.com")
-                    .firstName("Alex")
-                    .lastName("Alexeev")
-                    .birthDay(new Birthday(LocalDate.of(1989, 3, 10)))
-                    .role(Role.USER)
-                    .info("""
-                            {
-                            "name": "Alex",
-                            "id": 15 
-                            }                           
-                            """)
-                    .build();
-            session.saveOrUpdate(user);
-            session.getTransaction().commit();
+
+        try (SessionFactory sessionFactory = HibernateConfigurationUtil.buildSessionFactory()) {
+            try (Session session1 = sessionFactory.openSession()) {
+                session1.beginTransaction();
+                session1.saveOrUpdate(user);
+                session1.getTransaction().commit();
+            }
+            try (Session session2 = sessionFactory.openSession()) {
+                session2.beginTransaction();
+                user.setFirstName("Ollala");
+                session2.merge(user);
+//                session2.refresh(user);
+                session2.getTransaction().commit();
+            }
         }
     }
 }
---
+++ src/main/java/com/hibernate/util/HibernateConfigurationUtil.java
@@ -0,0 +1,19 @@
+package com.hibernate.util;
+
+import com.hibernate.coverter.BirthdayConvertor;
+import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
+import lombok.experimental.UtilityClass;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+@UtilityClass
+public class HibernateConfigurationUtil {
+    public static SessionFactory buildSessionFactory(){
+        Configuration configuration = new Configuration();
+        configuration.addAttributeConverter(new BirthdayConvertor(),true);
+        configuration.registerTypeOverride(new JsonBinaryType());
+        configuration.configure("hibernate.cfg.xml");
+        return configuration.buildSessionFactory();
+    }
+
+}

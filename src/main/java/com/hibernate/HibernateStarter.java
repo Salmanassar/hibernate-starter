57f4c329276f747e9faad268b05cf3169fc285a9
Professional <folknerolek@gmail.com>
2021-12-31 02:12:00 +0100

    add Logging for writing into console and file

--- src/main/java/com/hibernate/HibernateStarter.java
+++ src/main/java/com/hibernate/HibernateStarter.java
@@ -4,17 +4,16 @@ import com.hibernate.entity.Birthday;
 import com.hibernate.entity.Role;
 import com.hibernate.entity.User;
 import com.hibernate.util.HibernateConfigurationUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.sql.SQLException;
 import java.time.LocalDate;
 
+@Slf4j
 public class HibernateStarter {
-    private static final Logger logger = LoggerFactory.getLogger(HibernateStarter.class);
     public static void main(String[] args) throws SQLException {
         User user = User.builder()
                 .userName("alexx@gmail.com")
@@ -29,7 +28,7 @@ public class HibernateStarter {
                         }
                         """)
                 .build();
-        logger.info("The entity of User is a transient state, object {}", user);
+        log.info("The entity of User is a transient state, object {}", user);
 
 //If you need to use annotations without hibernate.cfg.xml use the java code below
 //configuration.addAnnotatedClass(User.class);
@@ -37,12 +36,12 @@ public class HibernateStarter {
         try (SessionFactory sessionFactory = HibernateConfigurationUtil.buildSessionFactory()) {
             try (Session session1 = sessionFactory.openSession()) {
                 Transaction transaction = session1.beginTransaction();
-                logger.trace("Transaction is created {}", transaction);
+                log.trace("Transaction is created {}", transaction);
                 session1.saveOrUpdate(user);
-                logger.trace("User is in persistent state {}, session {}", user, transaction);
+                log.trace("User is in persistent state {}, session {}", user, transaction);
                 session1.getTransaction().commit();
-            } catch (Exception exception){
-                logger.error("Error occurred {}",exception);
+            } catch (Exception exception) {
+                log.error("Error occurred {}", exception);
                 throw exception;
             }
             try (Session session2 = sessionFactory.openSession()) {
@@ -51,8 +50,8 @@ public class HibernateStarter {
                 session2.merge(user);
 //                session2.refresh(user);
                 session2.getTransaction().commit();
-            }catch (Exception exception) {
-                logger.error("Error occurred {}", exception);
+            } catch (Exception exception) {
+                log.error("Error occurred {}", exception);
                 throw exception;
             }
         }
